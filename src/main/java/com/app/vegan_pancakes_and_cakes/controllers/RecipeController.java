5d2507966d5bb326a84ddab42b5cca2d0b926dfe
Rexzinhoo <viniciussorgetzalves@gmail.com>
2024-11-13 10:49:48 -0300

    Tentativa de fazer a página da receita individual

--- src/main/java/com/app/vegan_pancakes_and_cakes/controllers/RecipeController.java
+++ src/main/java/com/app/vegan_pancakes_and_cakes/controllers/RecipeController.java
@@ -5,11 +5,9 @@ import com.app.vegan_pancakes_and_cakes.repositories.RecipeRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import java.util.List;
 
@@ -40,5 +38,28 @@ public class RecipeController {
             System.out.println(STR."Algo deu errado. Erro: \{error.getMessage()}");
         }
         return new ModelAndView("/recipe/home");
+    }
+
+    @PostMapping("/delete/{id}")
+    public String deleteRecipe(@PathVariable Long id, RedirectAttributes redirectAttributes) {
+        try {
+            recipeRepository.deleteById(id);
+            redirectAttributes.addFlashAttribute("message", "Receita deletada com sucesso!");
+        } catch (Exception e) {
+            redirectAttributes.addFlashAttribute("error", "Erro ao deletar receita.");
+        }
+        return "redirect:/recipe/list"; // Redireciona para a página de listagem ou outra que desejar
+    }
+
+    @GetMapping("/{id}")
+    public ModelAndView getRecipe(@PathVariable Long id, RedirectAttributes redirectAttributes){
+        try {
+            ModelAndView mv = new ModelAndView("recipe/recipe");
+            mv.addObject("recipe", recipeRepository.findById(id));
+            return mv;
+        } catch (Exception e) {
+            redirectAttributes.addFlashAttribute("error", "Erro ao deletar receita.");
+        }
+        return new ModelAndView("recipe/recipe");
     }
 }
--- src/main/java/com/app/vegan_pancakes_and_cakes/controllers/RecipeControllerRest.java
+++ src/main/java/com/app/vegan_pancakes_and_cakes/controllers/RecipeControllerRest.java
@@ -5,7 +5,10 @@ import com.app.vegan_pancakes_and_cakes.repositories.RecipeRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/recipe")
@@ -27,5 +30,16 @@ public class RecipeControllerRest {
             System.out.println(STR."Algo deu errado. Erro: \{error.getMessage()}");
         }
         return recipe;
+    }
+    @GetMapping("/a/{id}")
+    public Optional<Recipe> getRecipe(@PathVariable Long id, RedirectAttributes redirectAttributes){
+        try {
+            ModelAndView mv = new ModelAndView("recipe/recipe");
+            Optional<Recipe> recipe = recipeRepository.findById(id);
+            return recipe;
+        } catch (Exception e) {
+            redirectAttributes.addFlashAttribute("error", "Erro ao deletar receita.");
+        }
+        return Optional.of(new Recipe());
     }
 }
